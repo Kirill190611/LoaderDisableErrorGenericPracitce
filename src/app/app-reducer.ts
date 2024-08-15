fbab5100d9f93cf88014b996836c05a13ade9711
Kirill190611 <k.igorevitch88@yandex.ru>
2024-08-15 21:48:07 +0300

    Solution of Loader task

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -9,9 +9,18 @@ type AppStateType = typeof initialState
 
 export const appReducer = (state: AppStateType = initialState, action: ActionsType): AppStateType => {
   switch (action.type) {
+    case 'APP/SET-APP-STATUS':
+      return { ...state, status: action.status }
     default:
       return state
   }
 }
 
-type ActionsType = any
+export const setAppStatusAC = (status: RequestStatusType) => ({
+  type: 'APP/SET-APP-STATUS',
+  status,
+} as const)
+
+export type SetAppStatusType = ReturnType<typeof setAppStatusAC>
+
+type ActionsType = SetAppStatusType
